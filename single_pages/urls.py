cfedce3f9eca7edd491f16856fd8d78068e35e33
JiyunHJY <hwangjiyun7377@gmail.com>
2023-04-01 00:52:15 +0900

    템플릿 모듈화

--- single_pages/urls.py
+++ single_pages/urls.py
@@ -1,7 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.main),
+    path('about_me/', views.about_me),
+    path('', views.landing),
 ]
